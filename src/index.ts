556aa1fb025380338bbdebb93d205cb949ad022f
yamirmogollon <mogollonosorioyamir@gmail.com>
2026-05-29 08:08:46 -0500

    se agrego la linea numero 14

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from "express"
-import { RouterUsaurio } from "./Router/router.js" 
-import dotenv from "dotenv" 
-const app =  express() 
+import { RouterUsaurio } from "./Router/router.js"
+import dotenv from "dotenv"
+const app = express()
 let Port = 9000
-app.listen( Port,  ()=>{
-        console.log("en el puerto de " +  Port)
+app.listen(Port, () => {
+        console.log("en el puerto de " + Port)
 })
 app.use(RouterUsaurio)
-app.use( dotenv.config) 
+app.use(dotenv.config)
 
 
 console.log("mogollon")
+console.log("Estamosen la Developer --stack")
