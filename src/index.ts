a57e3ecb5eb5913d0600b076f60dfb23293f004d
yamirmogollon <mogollonosorioyamir@gmail.com>
2026-06-03 07:31:21 -0500

    data

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { RouterUsaurio } from "./Router/router.js"
 import dotenv from "dotenv"
 import { db } from "./db/db.js" 
 
+
 const conectarPasswird = async () => {
         try {
                 await db.authenticate()
@@ -24,13 +25,8 @@ app.listen(Port, () => {
 
 app.use(RouterUsaurio)
 app.use(dotenv.config)
-console.log("stagin Area")
 app.use(dotenv.config)
 console.log("Sataggin Ar --")
-console.log("Estamosen la Developer --stack")
-console.log("mogollon")
-console.log("Estamosen la Developer --stack")
-console.log("Estamosen la Developer --stack") 
 
 try {
         console.log(["Buenos Dias Peru"])
