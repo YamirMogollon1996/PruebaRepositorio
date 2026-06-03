3c4de5e8f4b0e234341dcbb340ec8a61ac1a8462
yamirmogollon <mogollonosorioyamir@gmail.com>
2026-06-03 08:01:19 -0500

    Se agrego la data

--- src/Features/UserFeature/Dtousuario.ts
+++ src/Features/UserFeature/Dtousuario.ts
@@ -1,10 +1,22 @@
-export class Usuario  {
-   public nombre : string  ;
-   public lastname :  string ; 
-   constructor(  nombre :  string , lastname: string) {
-             this.nombre =  nombre ,
-             this.lastname =  lastname
-   }
+
+
+export class Usuario {
+    public nombre: string;
+    public lastname: string;
+    constructor(nombre: string, lastname: string) {
+        this.nombre = nombre,
+            this.lastname = lastname
+    }
+    
+    ObtenerMisDatos() {
+        return this.lastname + ' - ' + this.nombre + 'Relaxo vision'
+    }
+    validateUsurio(nom: string, apel: string): Boolean {
+        if (this.nombre === nom && this.lastname === apel) {
+            return true
+        }
+        return false
+    }
 
 
 }
